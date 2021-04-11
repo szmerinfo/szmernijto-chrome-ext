8a3d02be25dc8d81cb837431fd4eba595d927ed7
Marcin Karwowski <marcin.karwowski@xentivo.pl>
2021-04-11 12:57:19 +0200

    Add automatic event trigger on form page

--- background.js
+++ background.js
@@ -8,9 +8,18 @@ chrome.webNavigation.onCompleted.addListener(function (details) {
             if (e.status === 200) return e.json();
             return false;
           }).then(data => {
-            document.getElementById('post-url').value = data && data.url ? data.url : previous_tab_data.url;
-            document.getElementById('post-title').value = data && data.title ? data.title : previous_tab_data.title;
-            document.querySelector('[id^="comment-textarea-"]').value = data && data.description ? data.description : '';
+            const el_url = document.getElementById('post-url');
+            el_url.value = data && data.url ? data.url : previous_tab_data.url;
+            el_url.dispatchEvent(new Event('input', { 'bubbles': true, 'cancelable': true }));
+  
+            const el_title = document.getElementById('post-title');
+            el_title.value = data && data.title ? data.title : previous_tab_data.title;
+            el_title.dispatchEvent(new Event('input', { 'bubbles': true, 'cancelable': true }));
+  
+            const el_description = document.querySelector('[id^="comment-textarea-"]');
+            el_description.value = data && data.description ? data.description : '';
+            el_description.dispatchEvent(new Event('input', { 'bubbles': true, 'cancelable': true }));
+            
             chrome.storage.sync.remove("previous_tab_data");
           });
         }
