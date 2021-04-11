ce644f5ae60aa6830fdaa170617dd51667173691
Marcin Karwowski <marcin.karwowski@xentivo.pl>
2021-04-11 09:53:52 +0200

    Optimization in form fill method and delete tab data after fill

--- background.js
+++ background.js
@@ -3,21 +3,17 @@ chrome.webNavigation.onCompleted.addListener(function (details) {
     target: { tabId: details.tabId },
     function: () => {
       chrome.storage.sync.get("previous_tab_data", ({ previous_tab_data }) => {
-        const el_url = document.getElementById('post-url');
-        const el_title = document.getElementById('post-title');
-        fetch(`https://szmer.info/iframely/oembed?url=${previous_tab_data.url}`).then(e => {
-          if (e.status === 200) return e.json();
-          return false;
-        }).then(data => {
-          if (data) {
-            el_url.value = data.url ? data.url : previous_tab_data.url;
-            el_title.value = data.title ? data.title : previous_tab_data.title;
-            document.querySelector('[id^="comment-textarea-"]').value = data.description ? data.description : '';
-          } else {
-            el_url.value = previous_tab_data.url;
-            el_title.value = previous_tab_data.title;
-          }
-        });
+        if (window.location.href.includes('szmer.info/create_post') && previous_tab_data) {
+          fetch(`https://szmer.info/iframely/oembed?url=${previous_tab_data.url}`).then(e => {
+            if (e.status === 200) return e.json();
+            return false;
+          }).then(data => {
+            document.getElementById('post-url').value = data && data.url ? data.url : previous_tab_data.url;
+            document.getElementById('post-title').value = data && data.title ? data.title : previous_tab_data.title;
+            document.querySelector('[id^="comment-textarea-"]').value = data && data.description ? data.description : '';
+            chrome.storage.sync.remove("previous_tab_data");
+          });
+        }
       });
     },
   });
